a0c9e4c9e4847630bcdcf1e8ee1ae484f38ef3d3
Minds05 <mindslink05@gmail.com>
2025-12-17 07:24:31 +0530

    Update Q6_Bank.java

--- Java - Lab/P6/Q6_Bank.java
+++ Java - Lab/P6/Q6_Bank.java
@@ -1,14 +1,46 @@
+import java.util.Scanner;
+
+
 abstract class Bank {
+    String name; int accNo; double bal;
+    Scanner sc = new Scanner(System.in);
+
+
+    void read() {
+        System.out.print("Enter Name, AccNo, Balance: ");
+        name = sc.next(); accNo = sc.nextInt(); bal = sc.nextDouble();
+    }
+    
+    void show(String bName, double rate) {
+        System.out.println("Name: " + name + " | Acc: " + accNo + " | Bal: " + bal);
+        System.out.println(bName + " (" + (rate * 100) + "%) Interest: " + (bal * rate));
+    }
     abstract void calcInterest();
 }
-class City extends Bank { void calcInterest() { System.out.println("City Bank: 5%"); } }
-class SBI extends Bank { void calcInterest() { System.out.println("SBI Bank: 6%"); } }
-class Canara extends Bank { void calcInterest() { System.out.println("Canara Bank: 7%"); } }
 
-class Q6_Bank {
+
+class CityBank extends Bank { void calcInterest() { show("City Bank", 0.05); } }
+class SBIBank extends Bank { void calcInterest() { show("SBI Bank", 0.06); } }
+class CanaraBank extends Bank { void calcInterest() { show("Canara Bank", 0.055); } }
+
+
+public class Prog6 {
     public static void main(String[] args) {
-        new City().calcInterest();
-        new SBI().calcInterest();
-        new Canara().calcInterest();
+        Scanner sc = new Scanner(System.in);
+        Bank b = null;
+        for (int ch = 0; ch != 4; ) {
+            System.out.print("\n1.City 2.SBI 3.Canara 4.Exit\nSelect: ");
+            ch = sc.nextInt();
+            if (ch == 4) break;
+            
+            if (ch == 1) b = new CityBank();
+            else if (ch == 2) b = new SBIBank();
+            else if (ch == 3) b = new CanaraBank();
+            else { System.out.println("Invalid"); continue; }
+
+
+            b.read();
+            b.calcInterest();
+        }
     }
 }
